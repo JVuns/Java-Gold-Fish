b4029215c776be40763214fb5a98922db23388e2
JVund <jevondanaristo72@gmail.com>
2021-12-26 15:12:16 +0800

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Java Gold Fish</title>
+        <link rel="stylesheet" href="CSS\menu-style.css">
+    </head>
+    <body>
+        <div>
+            <div class="header-container">
+                <div class="header-logo"><p>Java Gold Fish</p></div>
+                <div class="header-background"></div>
+                <div class="header-nav">
+                    <ul>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Gold Fish</a></li>
+                        <li><a hreg="">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="content-container">
+                <div class="Options">
+
+                </div>
+            </div>
+            <div class="footer">
+                <div class="form">
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
